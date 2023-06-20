2dd2b6b6a90ef816d905e274e6bb5da6311c6dc8
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-20 13:37:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const ele = document.getElementById("level").innerText;
+
+let arr = ele.split(" ");
+let value = parseInt(arr[1]);
+
+alert(`The level of the element is: ${value}`);
+
